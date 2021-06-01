19fc6a556145f673341aedb80b3ee316dec31f99
Brandon <bimcdona@ucsc.edu>
2021-06-01 16:33:36 -0700

    Groundwork laid for functionality of ending based on menu scene & alternate endings (clarifying previous broken commit message)

--- src/scenes/goodEnding.js
+++ src/scenes/goodEnding.js
@@ -3,7 +3,16 @@ class goodEnding extends Phaser.Scene {
         super("goodEnding");
     }
 
+    preload() {
+        this.cameras.main.fadeOut(0);
+        this.load.image('placeholder', './assets/Splash/TheThing.png');
+    }
     create() {
         this.add.text(0, 0, 'The Good Ending')
+        // this.endingImage = this.add.image(0, 0, 'placeholder').setOrigin(0,0).setDepth(-1);
+
+        this.time.delayedCall(750, () => {
+            this.cameras.main.fadeIn(1000);
+        });
     }
 }
