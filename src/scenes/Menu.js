c39c075553dc75129941a07a6ce934825a155dcc
henryTschudy <htschudy@ucsc.edu>
2021-06-03 11:35:39 -0700

    Implimented final menu design

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,7 +4,7 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('splash', './assets/Splash/menu.png');
+        this.load.spritesheet('splash', './assets/Splash/menu.png', {frameWidth: 576, frameHeight: 608, startFrame: 0, endFrame: 2});
         this.load.image('playEmpty', './assets/Splash/playEmpty.png');
         this.load.image('playHover', './assets/Splash/playHover.png');
         this.load.image('playHit', './assets/Splash/playHit.png');
@@ -25,7 +25,13 @@ class Menu extends Phaser.Scene {
         console.log(this.scene.get("hudScene"))
         console.log(this.scene.get("playScene"))
 
-        this.menu = this.add.image(0, 0, 'splash').setOrigin(0,0).setDepth(-1);
+        this.anims.create({
+            key: 'menuAnim',
+            frames: this.anims.generateFrameNumbers('splash', { zeroPad: 0, frames: [0, 1, 2, 0, 1, 2, 1, 0, 2, 1]}),
+            frameRate: frameRate
+        });
+
+        this.menu = this.add.sprite(0, 0, 'splash').setOrigin(0,0).setDepth(-1);
         this.play = this.add.sprite(144, roomHeight-61, 'playHover');
         this.help = this.add.sprite(roomWidth-155, roomHeight-61, 'helpEmpty');
         this.cameras.main.setBounds(0, 0, roomWidth, roomHeight);
@@ -44,6 +50,7 @@ class Menu extends Phaser.Scene {
     }
 
     update() {
+        this.menu.anims.play('menuAnim', true)
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
             if(this.currentSelect == "play"){
                 this.play.setTexture('playHit');
